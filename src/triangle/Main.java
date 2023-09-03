a88959d3c22adbe0387aba2a784317a572041178
JavaBeispielhaft <javabeispielhaft@gmail.com>
2023-09-03 20:07:38 +0200

    tutorial_3.0: Add simple triangle object example.

---
+++ src/triangle/Main.java
@@ -0,0 +1,11 @@
+package triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle0 = new Triangle();
+
+        triangle0.print();
+        triangle0.print();
+
+    }
+}
---
+++ src/triangle/Triangle.java
@@ -0,0 +1,9 @@
+package triangle;
+
+public class Triangle {
+    public void print(){
+        System.out.println("     #");
+        System.out.println("    ###");
+        System.out.println("   #####");
+    }
+}
